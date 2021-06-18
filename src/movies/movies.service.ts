01777689f750ad2e337f02719b4cd9b982b488d2
god9599 <59276134+god9599@users.noreply.github.com>
2021-06-18 23:26:03 +0900

    [Feat] 클래스로 영화 API 사용

---
+++ src/movies/movies.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+
+@Injectable()
+export class MoviesService {
+  private movies: Movie[] = [];
+
+  getAll(): Movie[] {
+    return this.movies;
+  }
+
+  getOne(id: string): Movie {
+    const movie = this.movies.find((movie) => movie.id === parseInt(id));
+    if (!movie) {
+      throw new NotFoundException('영화 id를 찾을 수 없습니다.');
+    }
+    return movie;
+  }
+
+  deleteOne(id: string) {
+    this.getOne(id);
+    this.movies = this.movies.filter((movie) => movie.id !== parseInt(id));
+  }
+
+  create(movieData) {
+    this.movies.push({
+      id: this.movies.length + 1,
+      ...movieData,
+    });
+  }
+
+  update(id: string, updateData) {
+    this.getOne(id);
+    this.deleteOne(id);
+    this.movies.push({ ...Movie, ...updateData });
+  }
+}
